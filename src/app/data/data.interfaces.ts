4d22a486a4a098b0e04b50ea8522119a207d7d0f
Andre Dantas <andre.dantas@globaleagle.com>
2018-04-04 17:16:51 -0400

    Separating interface in a different file

---
+++ src/app/data/data.interfaces.ts
@@ -0,0 +1,16 @@
+interface AboutMe {
+  name: string;
+  position: string;
+  photo: string;
+  title: string;
+  description: string;
+}
+
+
+export interface Data {
+  about_me: AboutMe;
+  skills: any;
+  projects: any;
+  jobs: any;
+  contact: any;
+}
--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -3,22 +3,7 @@ import { contact } from './contact';
 import { jobs } from './jobs';
 import { skills } from './skills';
 import { projects } from './projects';
-
-interface AboutMe {
-  name: string;
-  position: string;
-  photo: string;
-  title: string;
-  description: string;
-}
-interface Data {
-  about_me: AboutMe;
-  skills: any;
-  projects: any;
-  jobs: any;
-  contact: any;
-}
-
+import { Data } from './data.interfaces';
 
 export const data: Data = {
   about_me,
