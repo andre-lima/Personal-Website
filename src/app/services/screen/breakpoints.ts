c4c53e0c1cdda0ad5164972e6ef436087c45c4c2
Andre Dantas <andre.dantas@globaleagle.com>
2018-04-04 09:47:20 -0400

    Importing bulma and tweaking breakpoints

---
+++ src/app/services/screen/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  phone_small: 360,
+  phone: 480,
+  tablet: 768,
+  desktop: 1024,
+  desktop_big: 1216
+};
--- src/app/services/screen/screen.service.ts
+++ src/app/services/screen/screen.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
+import { breakpoints } from './breakpoints';
 
 @Injectable()
 export class ScreenService {
-  private breakpoints: Object = {
-    phone_small: 360,
-    phone: 480,
-    tablet: 768,
-    desktop: 1024,
-    desktop_big: 1216
-  }
 
   constructor() { }
 
   getCurrentBreakpoint(): string {
-    let currentBreakpoint = Object.keys(this.breakpoints)[0];
+    let currentBreakpoint = Object.keys(breakpoints)[0];
     const currentWidth = window.innerWidth;
 
-    for(let bp in this.breakpoints) {
-      if(currentWidth >= this.breakpoints[bp]) {
+    for (const bp in breakpoints ) {
+      if (currentWidth >= breakpoints[bp]) {
         currentBreakpoint = bp;
       }
     }
