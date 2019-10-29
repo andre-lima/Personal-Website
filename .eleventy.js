b978b90963f770d8a6b5d742a2862fe9c96745d4
Andre Dantas <andre.dantas.lima@gmail.com>
2019-10-28 23:09:59 -0400

    post styling and filters investigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
+// Import filters
+const dateFilter = require("./src/filters/date-filter.js");
+
+module.exports = function(config) {
+  // Filters
+  config.setPugOptions({
+    filters: {
+      dateFilter: dateFilter
+    }
+  });
+
+  config.addPassthroughCopy("src/assets");
 };
